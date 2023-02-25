5f92a9fd31c8927e5404aef5f37eeda6ed7af1d1
g-awmalik <malik.awais@gmail.com>
2023-02-24 21:37:32 -0800

    feat!: allows roles to be specified per folder per member (#55)
    Co-authored-by: Awais Malik <awmalik@google.com>

--- main.tf
+++ main.tf
@@ -18,15 +18,16 @@ locals {
   prefix       = var.prefix == "" ? "" : "${var.prefix}-"
   folders_list = [for name in var.names : google_folder.folders[name]]
   first_folder = local.folders_list[0]
-
-  name_role_pairs = setproduct(var.names, var.folder_admin_roles)
-  folder_admin_roles_map_data = zipmap(
-    [for pair in local.name_role_pairs : "${pair[0]}-${pair[1]}"],
-    [for pair in local.name_role_pairs : {
-      name = pair[0]
-      role = pair[1]
-    }]
-  )
+  folder_admin_roles_map_data = merge([
+    for name, config in var.per_folder_admins : {
+      for role in config.roles != null ? config.roles : var.folder_admin_roles : "${name}-${role}" =>
+      {
+        name    = name,
+        role    = role,
+        members = config.members,
+      }
+    }
+  ]...)
 }
 
 resource "google_folder" "folders" {
@@ -44,12 +45,8 @@ resource "google_folder_iam_binding" "owners" {
   folder   = google_folder.folders[each.value.name].name
   role     = each.value.role
 
-  members = compact(
-    concat(
-      split(",",
-        lookup(var.per_folder_admins, each.value.name, ""),
-      ),
-      var.all_folder_admins,
-    ),
+  members = concat(
+    each.value.members,
+    var.all_folder_admins,
   )
 }
